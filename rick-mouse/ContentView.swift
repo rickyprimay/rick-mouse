e5ca5fd610e227e3fedf1379ea0fdce60899924d
Ricky Primayuda Putra <rickyprima30@gmail.com>
2026-02-25 12:09:14 +0700

    initialize

---
+++ rick-mouse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rick-mouse
+//
+//  Created by Ricky Primayuda Putra on 25/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rick-mouse/rick_mouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  rick_mouseApp.swift
+//  rick-mouse
+//
+//  Created by Ricky Primayuda Putra on 25/02/26.
+//
+
+import SwiftUI
+
+@main
+struct rick_mouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
